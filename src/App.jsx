5d199e53f97580aeab8471e501f2ad5916ec83df
Ajit133 <dasajit697@gmail.com>
2025-07-13 12:20:48 +0600

    fix: correct route element for Register and ensure Login component is properly imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
 import Home from './pages/Home';
-import AuthPages from './pages/Register';
+import Login from './pages/Login';
+// import AuthPages from './pages/Register';
+import Register from './pages/Register';
 import { Routes, Route } from 'react-router-dom';
 function App() {
   
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/register' element={<AuthPages/>} />
+      <Route path='/register' element={<Register/>} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   )
 }
