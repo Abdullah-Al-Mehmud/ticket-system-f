b1e6b025eb7c6146e78b7e6431f1f33af6266964
Tanvir1407 <mt14050@gmail.com>
2025-07-22 10:08:48 +0600

    Update API base URL configuration to use environment variable

--- src/redux/app/api/apiSlice.js
+++ src/redux/app/api/apiSlice.js
@@ -3,8 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl:
-       'http://192.168.68.113:8000/api' ,  
+    baseUrl: import.meta.env.VITE_API_URL,
     prepareHeaders: (headers, { getState }) => {
       let token = localStorage.getItem('token');
       try {
